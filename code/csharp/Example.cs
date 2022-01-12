7cae5f4002d7f90e6cc61820c313a7da56dc56ce
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2022-01-12 14:54:03 +0000

    Triggered by direct commit. Origin: https://github.com/neo4j-contrib/sandbox-code-updater/commit/abc562ba5b5fb197c8cdbbe5cfb3d9576ecb134c

---
+++ code/csharp/Example.cs
@@ -0,0 +1,38 @@
+// install dotnet core on your system
+// dotnet new console -o .
+// dotnet add package Neo4j.Driver
+// paste in this code into Program.cs
+// dotnet run
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Neo4j.Driver;
+  
+namespace dotnet {
+  class Example {
+  static async Task Main() {
+    var driver = GraphDatabase.Driver("neo4j://<HOST>:<BOLTPORT>", 
+                    AuthTokens.Basic("<USERNAME>", "<PASSWORD>"));
+
+    var cypherQuery =
+      @"
+      MATCH (s:Stream{name:$streamer})<-[:VIP|:MODERATOR]-(user)
+      RETURN user.name as moderator_or_vip LIMIT 20
+      ";
+
+    var session = driver.AsyncSession(o => o.WithDatabase("neo4j"));
+    var result = await session.ReadTransactionAsync(async tx => {
+      var r = await tx.RunAsync(cypherQuery, 
+              new { streamer="ludwig"});
+      return await r.ToListAsync();
+    });
+
+    await session?.CloseAsync();
+    foreach (var row in result)
+      Console.WriteLine(row["moderator_or_vip"].As<string>());
+	  
+    }
+  }
+}
---
+++ code/python/example.py
@@ -0,0 +1,22 @@
+# pip3 install neo4j-driver
+# python3 example.py
+
+from neo4j import GraphDatabase, basic_auth
+
+driver = GraphDatabase.driver(
+  "neo4j://<HOST>:<BOLTPORT>",
+  auth=basic_auth("<USERNAME>", "<PASSWORD>"))
+
+cypher_query = '''
+MATCH (s:Stream{name:$streamer})<-[:VIP|:MODERATOR]-(user)
+RETURN user.name as moderator_or_vip LIMIT 20
+'''
+
+with driver.session(database="neo4j") as session:
+  results = session.read_transaction(
+    lambda tx: tx.run(cypher_query,
+                      streamer="ludwig").data())
+  for record in results:
+    print(record['moderator_or_vip'])
+
+driver.close()
