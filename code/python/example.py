5a8283667c76011e038e459fccbed089a9244fe1
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2023-09-21 11:26:36 +0000

    Triggered by direct commit. Origin: https://github.com/neo4j-contrib/sandbox-code-updater/commit/c25054b5806270c538866ae20a9097c3f6b30591

--- code/python/example.py
+++ code/python/example.py
@@ -1,22 +1,20 @@
-# pip3 install neo4j-driver
+# pip3 install neo4j
 # python3 example.py
 
 from neo4j import GraphDatabase, basic_auth
-
-driver = GraphDatabase.driver(
-  "neo4j://<HOST>:<BOLTPORT>",
-  auth=basic_auth("<USERNAME>", "<PASSWORD>"))
 
 cypher_query = '''
 MATCH (s:Stream{name:$streamer})<-[:VIP|:MODERATOR]-(user)
 RETURN user.name as moderator_or_vip LIMIT 20
 '''
 
-with driver.session(database="neo4j") as session:
-  results = session.read_transaction(
-    lambda tx: tx.run(cypher_query,
-                      streamer="ludwig").data())
-  for record in results:
-    print(record['moderator_or_vip'])
-
-driver.close()
+with GraphDatabase.driver(
+    "neo4j://<HOST>:<BOLTPORT>",
+    auth=("<USERNAME>", "<PASSWORD>")
+) as driver:
+    result = driver.execute_query(
+        cypher_query,
+        streamer="ludwig",
+        database_="neo4j")
+    for record in result.records:
+        print(record['moderator_or_vip'])
